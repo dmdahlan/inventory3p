e0071a782c3e528df0defb3de03d46971addaa1f
dmdahlan <muh_dahlan86@yahoo.co.id>
2021-01-13 16:33:48 +0800

    bug fix pembelian cash

--- app/Controllers/Rekap_pembeliancash.php
+++ app/Controllers/Rekap_pembeliancash.php
@@ -20,7 +20,6 @@ class Rekap_pembeliancash extends BaseController
         $report = $this->rekapcash->rekaphutang($tgl_awal, $tgl_akhir, $brand)->getResult();
 
         $data = array();
-        $no = @$_POST['start'];
         $jan = 0;
         $feb = 0;
         $mar = 0;
@@ -35,9 +34,8 @@ class Rekap_pembeliancash extends BaseController
         $des = 0;
         $grandtotal = 0;
         foreach ($report as $r) {
-            $no++;
             $row = array();
-            $row[] = $no;
+            $row[] = 'TOTAL PEMBELIAN CASH';
             $row[] = $this->rupiah($r->jan);
             $row[] = $this->rupiah($r->feb);
             $row[] = $this->rupiah($r->mar);
@@ -54,37 +52,8 @@ class Rekap_pembeliancash extends BaseController
             $total_bulan = $r->jan + $r->feb + $r->mar + $r->apr + $r->mei + $r->jun + $r->jul + $r->agt + $r->sep + $r->okt + $r->nop + $r->dess;
             $row[] = $this->rupiah($total_bulan);
 
-            $jan += $r->jan;
-            $feb += $r->feb;
-            $mar += $r->mar;
-            $apr += $r->apr;
-            $mei += $r->mei;
-            $jun += $r->jun;
-            $agt += $r->agt;
-            $sep += $r->sep;
-            $okt += $r->okt;
-            $nop += $r->nop;
-            $des += $r->dess;
-            $grandtotal += $total_bulan;
-
             $data[] = $row;
         }
-        $data[] = array(
-            'TOTAL',
-            $this->rupiah($jan),
-            $this->rupiah($feb),
-            $this->rupiah($mar),
-            $this->rupiah($apr),
-            $this->rupiah($mei),
-            $this->rupiah($jun),
-            $this->rupiah($jul),
-            $this->rupiah($agt),
-            $this->rupiah($sep),
-            $this->rupiah($okt),
-            $this->rupiah($nop),
-            $this->rupiah($des),
-            $this->rupiah($grandtotal),
-        );
         $output = array(
             "draw" => @$_POST['draw'],
             "data" => $data,
